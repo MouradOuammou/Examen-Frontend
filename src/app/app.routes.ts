a1f6b4fdf471fe84a49c5331c431dd87d3ff7748
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-19 11:26:07 +0100

    configuration de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '//name', pathMatch: 'full' },
-
+const routes: Routes = [
+  { path: 'clients', redirectTo : 'ClientsComponent', pathMatch : 'full' },
+  { path: 'credits', component: CreditsComponent },
+  { path: 'remboursements', component: RemboursementsComponent },
+  { path: '', redirectTo: '/clients', pathMatch: 'full' },
+  { path: '**', redirectTo: '/clients' }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
